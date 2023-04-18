53f7cadf6c44606979bc038d0ddef2103706ad72
martaloring <94187225+martaloring@users.noreply.github.com>
2023-04-18 13:36:59 +0200

    Add files via upload

--- chat_pkg/chat_pkg/poses_mng.py
+++ chat_pkg/chat_pkg/poses_mng.py
@@ -5,8 +5,8 @@ from rclpy.exceptions import ROSInterruptException
 #sys.path.append('/home/mapir/ros2_ws/src/chat_pkg')
 import threading
 import numpy as np
-from std_msgs.msg import String
-from geometry_msgs.msg import PoseArray, PoseStamped
+from std_msgs.msg import String, Bool
+from geometry_msgs.msg import PoseArray, PoseStamped, Pose
 from rclpy.action import ActionClient
 from nav2_msgs.action import NavigateToPose
 
@@ -15,16 +15,26 @@ class SocialPatrol(Node):
         super().__init__('social_patrol')
 
         self._pub_patrol = self.create_publisher(PoseStamped, "/filtered_pose", 1)   
-        self._sub_openpose = self.create_subscription(PoseArray, "/poses_topic", self.callback_OP, 1) 
-        self.var = False
+        self._sub_openpose = self.create_subscription(PoseArray, "/poses_topic", self.callback_OP, 1)
+        self._sub_compute = self.create_publisher(Bool, "/compute_pose", self.callback_compute, 1) 
+        self.send = False
+
+    def callback_compute(self,msg):
+        print("callback compute")
+        self.send = True
 
     def callback_OP(self, msg):  # esto nodo calcula cual es la pose de "delante de la persona" a partir de la pose de la persona
         print("callback openpose")
-        self.var = True
-        #if()
-        pose = PoseStamped()
-        pose.pose = msg.poses[0]
-        pose.header.frame_id = 'map'
+        if(len(msg.poses) > 0 and self.send):
+            self.send = False
+            pose = PoseStamped()
+            p_goal = self.pose_front(msg.poses[0])
+            pose.pose = p_goal
+            pose.header.frame_id = 'map'
+
+    def pose_front(self, p_user):
+        p_goal = Pose()
+        return p_goal
         
 
     def main_loop(self): 
--- chat_pkg/chat_pkg/social_patrol.py
+++ chat_pkg/chat_pkg/social_patrol.py
@@ -5,41 +5,47 @@ from rclpy.exceptions import ROSInterruptException
 #sys.path.append('/home/mapir/ros2_ws/src/patrol')
 import threading
 import numpy as np
-from std_msgs.msg import String
+from std_msgs.msg import String, Bool
 from geometry_msgs.msg import PoseArray, PoseStamped
 from rclpy.action import ActionClient
 from nav2_msgs.action import NavigateToPose
 from patrol.action import PatrolTimes
 from action_msgs.msg import GoalStatus
+import rclpy.clock
+import rclpy.duration
 
 class SocialPatrol(Node):
     def __init__(self):
         super().__init__('social_patrol')
 
         self._pub_tts = self.create_publisher(String, "/input_text", 1)
+        self._pub_compute = self.create_publisher(Bool, "/compute_pose", 1)
         self._sub_asr = self.create_subscription(String, "/asr_info", self.callback_ASR, 1)      
         self._sub_openpose = self.create_subscription(PoseStamped, "/filtered_pose", self.callback_OP, 1) 
         self.patrullando = False
         self.goToUser = False
         self.stopPatrol = False
+        self.startPatrol = True
         self.var = True
         self.patrol_client = ActionClient(self, PatrolTimes, '/patrol_times')
         self.navToPose_client = ActionClient(self, NavigateToPose, '/navigate_to_pose')
+        self.past = self.get_clock().now()
+        self.compute_pose = False
 
     # ASR CALLBACK
-    def callback_ASR(self, msg):
+    def callback_ASR(self, msg):        # aqui mando un mensaje si ya ha terminado la conversacion y quiero volver a empezar el patrol 
         print("callback ASR")
-
-    ##########################################################################################
-    # 
-    #           NAVIGATE_TO_POSE ACTION, FEEDBACK CALLBACK AND RESULT CALLBACK
-    # 
-    # ########################################################################################        
 
     def callback_OP(self, msg):
         print("callback openpose")
         self.stopPatrol= True
         self.next_pose = msg
+
+    #####################################################################################################
+    # 
+    #       NAVIGATE_TO_POSE ACTION //// SEND GOAL & RESPONSE GOAL & FEEDBACK CALLBACK & RESULT GOAL 
+    # 
+    # ###################################################################################################
         
     def start_goToUser(self):
         goal_msg = NavigateToPose.Goal()
@@ -49,12 +55,7 @@ class SocialPatrol(Node):
         print("navigate_to_pose action server is ready")
 
         self._send_goal_user = self.navToPose_client.send_goal_async(goal_msg, feedback_callback = self.callback_feedback_goToUser)
-        self._send_goal_user.add_done_callback(self.callback_response_goToUser)   # callback para recibir feedback del action server 
-
-    def callback_feedback_goToUser(self, feedback_msg):
-        print("callback feedback go to user")
-        feedback = feedback_msg.feedback
-        self.get_logger().info('Received feedback, distance remaining: {0}'.format(feedback.distance_remaining))
+        self._send_goal_user.add_done_callback(self.callback_response_goToUser)   # callback para saber si el goal se ha aceptado o no
 
     def callback_response_goToUser(self, future):
         goal_handle_op = future.result()
@@ -67,24 +68,32 @@ class SocialPatrol(Node):
             self.get_logger().info('Goal (goToUser) rejected :(')
             return
 
+    def callback_feedback_goToUser(self, feedback_msg):
+        print("callback feedback go to user")
+        feedback = feedback_msg.feedback
+        self.get_logger().info('Received feedback, distance remaining: {0}'.format(feedback.distance_remaining))
+
     def callback_result_goToUser(self, future):
         print("¿he llegado?")
 
-        #     print("exito go to user!")
-        #     pub_msg = String()
-        #     pub_msg.data = "He llegado a la persona"
-        #     self._pub_tts.publish(pub_msg)
+        # si encuentro alguna variable que se de cuenta de que no he llegado, puedo volver a hacer (self.goTouser = True)
+
+        print("exito go to user!")
+        pub_msg = String()
+        pub_msg.data = "Hola, soy Sancho el robot de servicio. Estoy disponible para lo que necesites. Si quieres hacerme alguna pregunta di: Hola, Sancho."
+        self._pub_tts.publish(pub_msg)
 
     ##########################################################################################
     # 
-    #            PATROL_TIMES ACTION, START, STOP, FEEDBACK CALLBACK AND RESULT CALLBACK
+    #  PATROL_TIMES ACTION //// SEND GOAL & RESULT GOAL //// CANCEL GOAL & RESULT CANCELLING
     # 
     # ########################################################################################
     
     # START patrol and while patrol:
     def start_patrol(self):
-        print("ey")
+        print("start patrol")
         self.patrullando = True
+        self.compute_pose = False
         goal_msg = PatrolTimes.Goal()
         goal_msg.times = 2
         self._send_goal_patrol = self.patrol_client.send_goal_async(goal_msg)
@@ -95,6 +104,7 @@ class SocialPatrol(Node):
         self.goal_handle_pa = future.result()
         if(self.goal_handle_pa.accepted):
             self.get_logger().info('Goal (patrol) accepted :)')
+            self.past = self.get_clock().now()
 
         else:
             self.get_logger().info('Goal (patrol) rejected :(')
@@ -109,20 +119,26 @@ class SocialPatrol(Node):
 
     def goal_canceled_callback(self, future):
         print("cancel patrol callback")
+        sec = rclpy.duration.Duration(seconds=2) 
+        rclpy.clock.Clock().sleep_for(sec)
         self.goToUser = True
-        # cancel_handle_pa = future.result()
-        # if(cancel_handle_pa.response == 0):
-        #     self.get_logger().info('Cancel (patrol) accepted')
 
-        # else:
-        #     self.get_logger().info('Cancel (patrol) rejected')
-        #     return
+    #####################################################################################################
 
     def main_loop(self): 
         threading.Thread(target = rclpy.spin,args = (self,), daemon=True).start()
-        self.start_patrol()
 
         while (rclpy.ok()):
+            if(self.startPatrol):
+                self.start_patrol()
+                self.startPatrol = False
+            if(self.patrullando and not self.compute_pose):
+                now = self.get_clock().now()
+                if((now - self.past).nanoseconds*1e-9) > 8:
+                    self.compute_pose = True
+                    msg = Bool()
+                    msg.data = True
+                    self._pub_compute.publish(msg)
             if(self.stopPatrol):
                 self.stop_patrol()
                 self.stopPatrol = False
